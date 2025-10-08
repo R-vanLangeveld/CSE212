775a9aa215888ed01bbbd5cd0931fa35595dbf41
R-vanLangeveld <rvanlangeveld@byupathway.edu>
2025-10-08 16:53:30 -0600

    Completed team activity

--- week06/code/Node.cs
+++ week06/code/Node.cs
@@ -17,29 +17,93 @@ public class Node
         {
             // Insert to the left
             if (Left is null)
+            {
                 Left = new Node(value);
+            }
             else
+            {
                 Left.Insert(value);
+            }
         }
-        else
+        else if (value > Data)
         {
             // Insert to the right
             if (Right is null)
+            {
                 Right = new Node(value);
+            }
             else
+            {
                 Right.Insert(value);
+            }
         }
     }
 
     public bool Contains(int value)
     {
         // TODO Start Problem 2
-        return false;
+        if (value == Data)
+        {
+            return true;
+        }
+        else if (value < Data)
+        {
+            if (Left is null)
+            {
+                return false;
+            }
+            else
+            {
+                return Left.Contains(value);
+            }
+        }
+        else if (value > Data)
+        {
+            if (Right is null)
+            {
+                return false;
+            }
+            else
+            {
+                return Right.Contains(value);
+            }
+        }
+        else
+        {
+            return false;
+        }
     }
 
     public int GetHeight()
     {
         // TODO Start Problem 4
-        return 0; // Replace this line with the correct return statement(s)
+        var leftSubtree = 0;
+        var rightSubtree = 0;
+
+        if (Left is not null)
+        {
+            leftSubtree = Left.GetHeight() + 1;
+        }
+        if (Right is not null)
+        {
+            rightSubtree = Right.GetHeight() + 1;
+        }
+
+        if (leftSubtree > rightSubtree)
+        {
+            return leftSubtree;
+        }
+        else if (leftSubtree < rightSubtree)
+        {
+            return rightSubtree;
+        }
+        else if (leftSubtree == rightSubtree && Left is not null && Right is not null)
+        {
+            return (leftSubtree + rightSubtree) / 2;
+        }
+        else
+        {
+            return 1;
+        }
     }
 }
